705bd4b7dbf4c66b163e003ae0f21be695639187
NarminAlakbarova <126705252+NarminAlakbarova@users.noreply.github.com>
2023-04-07 05:57:32 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,42 +1,20 @@
-// for(i=0; i<=10; i++){
-//     console.log(`${i}*${i}= ${i*i}`);
-// }
+// test1
+// let text="there";
+// console.log(`hello ${3+8} + 5 +${'i am fine'}`);
+// console.log(`hello ${`3+8`+2}`);
+// console.log(`hello ${"text"}`);
+// console.log(`hello ${text}`);
+
+// test2
+// let firstName="Narmin";
+// let lastName="Alakbarova";
+// let age=19;
+// let cityName="Gence";
+// let uni="GDU";
+// let allInfoAboutMe=`${firstName} ${lastName} ${age} ${cityName} ${uni}`
+// console.log(allInfoAboutMe);
+
+// test3
 
 
-// // task1
-// let myName="Narmin";
-//  for( i=0;i<myName.length;i++){
-//     console.log(myName);
-//  }
-
-// // task2
-//  for(i=0;i<=8; i++){
-// if (i%2==0){
-//     console.log(i);
-// }
-
-//  }
-      
- 
-// //  task3
-// for(i=0; i<=7 ;i++){
-
-//     if(i%2==0){
-
-//         if(i==0){
-//             continue
-//          }
-//        console.log(`even ${i}`);
-     
-//     }
-//     else{
-//        console.log(`odd ${i}`);
-//     }
-// }
-
-
-let myName=prompt(`Name?`, ``);
-let message=(myName=="Narmin") ? "Hello" :
-"bye";
-alert( message );
 
