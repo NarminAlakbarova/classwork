705e4030fdb0cff097c42f5dc2cc9a8daef812a5
NarminAlakbarova <725e5lb@code.edu.az>
2023-04-27 01:01:42 +0400

    some changes 4.26Dom-Bom-form task

--- 4.26DOM-BOM-form/main.js
+++ 4.26DOM-BOM-form/main.js
@@ -14,6 +14,7 @@ let nextPageBtn = document.createElement("button");
 document.body.append(startBtn);
 document.body.append(time);
 document.body.append(form);
+document.body.append(nextPageBtn);
 form.append(nameLabel);
 form.append(nameInp);
 form.append(surLabel);
@@ -22,7 +23,6 @@ form.append(checkInp);
 form.append(radioInp1);
 form.append(radioInp2);
 form.append(submitBtn);
-document.body.append(nextPageBtn);
 
 nameLabel.innerHTML = "<strong>Name</strong>";
 surLabel.innerHTML = "<strong>Surname</strong>";
@@ -34,21 +34,20 @@ let count = 15;
 time.innerHTML = `00:${count}`;
 let interval;
 startBtn.addEventListener("click", function () {
+  count = 15;
   startBtn.setAttribute("disabled", "");
   submitBtn.removeAttribute("disabled");
   interval = setInterval(() => {
-    time.innerHTML = `00:${count}`;
+    time.innerHTML = `00:${--count}`;
     time.style.color = "blue";
     time.style.borderColor = "blue";
-    --count;
-    if (count == 10) {
+
+    if (count <= 10) {
       time.style.borderColor = "red";
-      time.style.Color = "red";
+      time.style.color = "red";
     }
     if (count == 0) {
       time.innerHTML = "You missed :(";
-      time.style.color = "red";
-      time.style.borderColor = "red";
       clearInterval(interval);
       submitBtn.setAttribute("disabled", "");
       startBtn.removeAttribute("disabled");
@@ -77,6 +76,12 @@ radioInp2.type = "radio";
 radioInp2.style.accentColor = "red";
 radioInp2.required = true;
 
+nameInp.placeholder = "Enter your name...";
+nameInp.style.padding = "5px";
+
+surInp.placeholder = "Enter your surname...";
+surInp.style.padding = "5px";
+
 form.style.display = "flex";
 form.style.flexDirection = "column";
 form.style.justifyContent = "center";
@@ -85,8 +90,8 @@ form.style.height = "90vh";
 form.style.border = "1px dotted black";
 form.style.rowGap = "10px";
 
-time.style.height = "30px";
 time.style.width = "90px";
+time.style.padding = "10px";
 time.style.border = "1px solid black";
 time.style.textAlign = "center";
 
@@ -94,4 +99,3 @@ nameInp.required = true;
 surInp.required = true;
 radioInp1.name = "n";
 radioInp2.name = "n";
-// time.innerHtml=`00:${count}`
