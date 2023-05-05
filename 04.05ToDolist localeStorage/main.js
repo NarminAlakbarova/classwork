16dd219ce265d5740993856f49f58d433d90bbe1
NarminAlakbarova <735e5lb@code.edu.az>
2023-05-06 00:42:25 +0400

    update ToDo List

--- 04.05ToDolist localeStorage/main.js
+++ 04.05ToDolist localeStorage/main.js
@@ -17,7 +17,6 @@ input.addEventListener("input", function (e) {
 function create() {
   listGroup.innerHTML = "";
   todos.forEach((item) => {
-   
     listGroup.innerHTML += `
     <li class="list-group-item d-flex justify-content-between my-4">
             <div>
@@ -26,53 +25,64 @@ function create() {
                 type="checkbox"
                 value=""
                 aria-label="..."
+                id="${item.id}" onclick=check(${item.id}) ${
+      item.checked && "checked"
+    }
+               
               />
-              <label for="" id="txt">${item.todoText}</label>
+              <label for="" class="${item.checked && "line"}">${
+      item.todoText
+    }</label>
             </div>
             <div>
-              <button type="button" class="btn btn-success" onclick=edit(${item.id})>edit</button>
-              <button type="button" class="btn btn-danger" onclick=remove(${item.id})>delete</button>
+              <button type="button" class="btn btn-success" onclick=edit(${
+                item.id
+              })>edit</button>
+              <button type="button" class="btn btn-danger" onclick=remove(${
+                item.id
+              })>delete</button>
             </div>
           </li>
     
     
     `;
- 
   });
 }
-addBtn.onclick=function() {
+
+function check(id) {
+  let todosObj = todos.find((item) => item.id == id);
+  let findIndex = todos.findIndex((item) => item.id == id);
+  todosObj.checked ? (todosObj.checked = false) : (todosObj.checked = true);
+  todos.splice(findIndex, 1, todosObj);
+  localStorage.setItem("todoList", JSON.stringify(todos));
+  create();
+}
+let addvalue = false;
+addBtn.addEventListener("click", function () {
+  addBtn.innerText = "Add";
   let obj = {
     id: Date.now(),
     todoText: input.value,
-   
   };
-  todos.push(obj);
-  localStorage.setItem("todoList", JSON.stringify(todos));
-  create();
-  input.value = "";
-};
-
-
-function remove(id) {
-  todos = todos.filter((obj) => obj.id != id);
-  localStorage.setItem("todoList", JSON.stringify(todos));
-  create();
-}
-
-
+  if (!addvalue) {
+    todos.push(obj);
+    localStorage.setItem("todoList", JSON.stringify(todos));
+    create();
+    input.value = "";
+  }
+  addBtn.disabled = true;
+});
 function edit(id) {
-  let edit = todos.find((obj) => obj.id == id);
+  let edit = todos.filter((obj) => obj.id == id)[0];
   input.value = edit.todoText;
   addBtn.innerText = "Edit";
-  addBtn.onclick = function () {
+  if (addvalue) {
     let finish = todos.find((obj) => obj.id == id);
     finish.todoText = input.value;
     localStorage.setItem("todoList", JSON.stringify(todos));
     create();
-    addBtn.innerText="Add"
-    input.value=""
-  };
-
+    input.value = "";
+  }
 }
 
 listGroup.addEventListener("click", function (e) {
@@ -81,6 +91,12 @@ listGroup.addEventListener("click", function (e) {
     parentLi.classList.toggle("completed");
   }
 });
-window.onload = function() {
+function remove(id) {
+  todos = todos.filter((obj) => obj.id != id);
+  localStorage.setItem("todoList", JSON.stringify(todos));
   create();
 }
+
+window.onload = function () {
+  create();
+};
