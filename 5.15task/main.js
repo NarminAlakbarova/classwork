8ac5bc2914077d94e762b0a168c5e0f44f9d5022
NarminAlakbarova <126705252+NarminAlakbarova@users.noreply.github.com>
2023-05-15 05:50:36 -0700

    Add files via upload

---
+++ 5.15task/main.js
@@ -0,0 +1,5 @@
+let menuicon=document.querySelector(".fa-bars")
+let navMobile=document.querySelector(".nav-mobile")
+menuicon.addEventListener("clcik",function(){
+    navMobile.classList.toggle("show")
+})
